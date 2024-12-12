348d576ad8ebdb8ca42ea4ad5074465f53d8678c
wuliya <3369906077@qq.com>
2024-12-13 02:38:20 +0800

    🔖 feat: 新增默认表情包和表情黑名单设置
    - 在 CHANGELOG.md 中添加了 1.1.4 版本的更新内容
    - 更新了 package.json 中的版本号至 1.1.4

--- apps/meme.js
+++ apps/meme.js
@@ -2,53 +2,53 @@ import { Config } from '../components/index.js'
 import { Meme, Rule } from '../models/index.js'
 
 export class meme extends plugin {
-  constructor() {
+  constructor () {
     super({
       name: '清语表情:表情包生成',
       event: 'message',
-      priority: 200,
+      priority: -Infinity,
       rule: []
-    });
+    })
 
     if (Meme.keyMap) {
       this.rule = Object.keys(Meme.keyMap).map(keyword => {
-        const dynamicPrefix = this.getPrefix();
+        const dynamicPrefix = this.getPrefix()
         return {
           reg: new RegExp(`${dynamicPrefix}(${keyword})(.*)`, 'i'),
           fnc: 'meme'
-        };
-      });
+        }
+      })
     } else {
-      logger.error('[清语表情] 初始化失败');
+      logger.error('[清语表情] 初始化失败')
     }
   }
 
-  getPrefix() {
-    const sharpPrefix = Config.meme.forceSharp ? '#' : '#?';
-    const defaultPrefix = Config.meme.default ? '' : '清语表情';
-    return `${sharpPrefix}${defaultPrefix}`;
+  getPrefix () {
+    const sharpPrefix = Config.meme.forceSharp ? '#' : '#?'
+    const defaultPrefix = Config.meme.default ? '' : '清语表情'
+    return `${sharpPrefix}${defaultPrefix}`
   }
 
-  async meme(e) {
-    const message = e.msg.trim();
-    const prefix = this.getPrefix();
-    const prefixRegex = new RegExp(`^${prefix}`);
+  async meme (e) {
+    const message = e.msg.trim()
+    const prefix = this.getPrefix()
+    const prefixRegex = new RegExp(`^${prefix}`)
 
     const matchedKeyword = Object.keys(Meme.keyMap).find(key =>
       prefixRegex.test(message) && new RegExp(key, 'i').test(message)
-    );
+    )
 
-    if (!matchedKeyword || 
+    if (!matchedKeyword ||
         !Meme.getKey(matchedKeyword) ||
         Config.meme.blackList.includes(matchedKeyword.toLowerCase()) ||
         Config.meme.blackList.includes(Meme.getKey(matchedKeyword).toLowerCase()) ||
         !Meme.getInfo(Meme.getKey(matchedKeyword))) {
-      return true; 
+      return true
     }
 
-    const memeKey = Meme.getKey(matchedKeyword);
-    const memeInfo = Meme.getInfo(memeKey);
-    const userText = message.replace(prefixRegex, '').trim().replace(new RegExp(`^${matchedKeyword}`, 'i'), '').trim();
-    return await Rule.meme(e, memeKey, memeInfo, userText);
+    const memeKey = Meme.getKey(matchedKeyword)
+    const memeInfo = Meme.getInfo(memeKey)
+    const userText = message.replace(prefixRegex, '').trim().replace(new RegExp(`^${matchedKeyword}`, 'i'), '').trim()
+    return await Rule.meme(e, memeKey, memeInfo, userText)
   }
 }

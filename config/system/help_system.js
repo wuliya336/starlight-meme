fd7108839db15a70113d80a0f3fc15b2fe4cf31c
wuliya <3369906077@qq.com>
2024-12-15 17:36:33 +0800

    feat(help-system): 更新帮助列表图标和命令
    - 更新了多个命令的图标编号
    - 添加了新的更新表情资源命令
    - 优化了现有命令的描述信息

--- config/system/help_system.js
+++ config/system/help_system.js
@@ -24,25 +24,25 @@ export const helpList = [
     group: "拓展命令",
     list: [
       {
-        icon: 81,
+        icon: 161,
         title: "#清语表情列表",
         desc: "获取表情列表",
       },
       {
-        icon: 81,
+        icon: 90,
         title: "#清语表情搜索xx",
         desc: "搜指定的表情",
       },
       {
-        icon: 81,
+        icon: 75,
         title: "#清语表情xx详情",
         desc: "获取表情详情",
       },
       {
-        icon: 81,
+        icon: 71,
         title: "xx",
-        desc: "如喜报xx",
-      }
+        desc: "如喜报xx (参数使用#, 多段文本使用/, 指定用户头像使用@+qq号)",
+      },
     ],
   },
   {
@@ -53,6 +53,11 @@ export const helpList = [
         icon: 95,
         title: "#清语表情(插件)(强制)更新",
         desc: "更新插件本体",
+      },
+      {
+        icon: 81,
+        title: "#清语表情(强制)更新资源",
+        desc: "更新表情资源",
       },
       {
         icon: 85,
